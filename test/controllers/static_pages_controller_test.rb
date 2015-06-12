eb2c02babeba1e4dbe3755cb1fc880db39ee5902
Pham Thi Thu Trang <pham.thi.thu.trang@framgia.com>
2015-06-12 10:02:21 +0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Deahan-Minguk-Manse"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Deahan-Minguk-Manse"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Deahan-Minguk-Manse"
+  end
 end
