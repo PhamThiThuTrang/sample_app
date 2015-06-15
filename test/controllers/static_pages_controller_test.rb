899eaa9bc3403fcfd370ec76a2d6df15aca39b12
Pham Thi Thu Trang <pham.thi.thu.trang@framgia.com>
2015-06-15 10:58:33 +0700

    Finish layout anh routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,18 +5,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Deahan-Minguk-Manse"
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Deahan-Minguk-Manse"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Deahan-Minguk-Manse"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample"
   end
 end
