5ebf7b5d60371d5b4102d739830ef112a92a2d2f
Pham Thi Thu Trang <pham.thi.thu.trang@framgia.com>
2015-06-16 08:35:38 +0700

    Following user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users
   resources :entries,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
